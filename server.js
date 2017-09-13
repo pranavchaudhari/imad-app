0e0bf58a9a250804f3e2ccdf1ff9b03bdcde4967
pranavchaudhari <30976150+pranavchaudhari@users.noreply.github.com>
2017-09-13 20:33:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+  res.send('Article one will be served');
 });
 
 app.get('/ui/madi.png', function (req, res) {
